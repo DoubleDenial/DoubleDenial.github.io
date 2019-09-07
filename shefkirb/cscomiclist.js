dab69ab99d1e45489126f12ebb69d5fd8c2747e0
DoubleDenial <26236076+DoubleDenial@users.noreply.github.com>
2019-09-06 20:37:09 -0400

    coming soon (;

---
+++ shefkirb/cscomiclist.js
@@ -0,0 +1,45 @@
+var comicsrcs = [
+"https://shefwerld.github.io/shefkirb/comics/media/apple/1.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/2.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/3.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/4.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/5.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/6.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/7.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/8.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/9.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/10.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/11.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/12.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/13a.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/14.png",
+"https://shefwerld.github.io/shefkirb/comics/media/apple/15.png"];
+var comicnames = [
+"No Arms",
+"Philosophy Vs. Dinosaurs",
+"Confession",
+"Dab Day",
+"The Sun",
+"Creative Drought",
+"Apple's Hearty Christmas Twinkies",
+"Dr. Bach Mann",
+"Why You So Serious?",
+"Cookies",
+"Snopes",
+"Gullible",
+"Yanny",
+"Border Control",
+"Smoking"];
+var x = Math.round(Math.random())
+if (x == 1) {
+	comicnames[12]="Yanny";
+	comicsrcs[12]="https://shefwerld.github.io/shefkirb/comics/media/apple/13a.png";
+} else {
+	comicnames[12]="Laurel"
+	comicsrcs[12]="https://shefwerld.github.io/shefkirb/comics/media/apple/13b.png";
+}
+
+function callComic(number) {
+	number = number - 1;
+	loadComic(number);
+}
