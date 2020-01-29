8c8b6c13420c0b9e86b09245aee95dacf1b0b8c7
DoubleDenial <26236076+DoubleDenial@users.noreply.github.com>
2020-01-28 19:02:50 -0500

    Update cscomiclist.js

--- shefkirb/cscomiclist.js
+++ shefkirb/cscomiclist.js
@@ -1,27 +1,27 @@
 var comicsrcs = [
-"https://shefwerld.github.io/shefkirb/comics/media/apple/1.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/2.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/3.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/4.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/5.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/6.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/7.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/8.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/9.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/10.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/11.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/12.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/13a.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/14.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/15.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/16.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/17.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/18.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/19.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/20.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/21.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/22.png",
-"https://shefwerld.github.io/shefkirb/comics/media/apple/23.png"];
+"https://shefwerld.github.io/shefnews/comics/media/apple/1.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/2.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/3.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/4.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/5.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/6.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/7.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/8.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/9.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/10.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/11.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/12.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/13a.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/14.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/15.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/16.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/17.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/18.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/19.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/20.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/21.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/22.png",
+"https://shefwerld.github.io/shefnews/comics/media/apple/23.png"];
 var comicnames = [
 "No Arms",
 "Philosophy Vs. Dinosaurs",
@@ -48,10 +48,10 @@ var comicnames = [
 "Vegan"];
 if (Math.round(Math.random()) == 1) {
 	comicnames[12]="Yanny";
-	comicsrcs[12]="https://shefwerld.github.io/shefkirb/comics/media/apple/13a.png";
+	comicsrcs[12]="https://shefwerld.github.io/shefnews/comics/media/apple/13a.png";
 } else {
 	comicnames[12]="Laurel"
-	comicsrcs[12]="https://shefwerld.github.io/shefkirb/comics/media/apple/13b.png";
+	comicsrcs[12]="https://shefwerld.github.io/shefnews/comics/media/apple/13b.png";
 }
 
 function callComic(number) {
